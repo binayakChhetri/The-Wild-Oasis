b091a0c784421ed18cc9d76bb791a47a706c3ee8
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-09-18 12:23:05 +0545

    Built sidebar and main

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,15 +1,27 @@
+// import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
-const Aside = styled.aside`
+const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1/-1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <Aside>Sidebar</Aside>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
