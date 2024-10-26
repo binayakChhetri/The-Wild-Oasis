71eb358c0c6494c039d692543724063aa087edd1
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-10-26 16:05:35 +0545

    Feature added: User can do update name, avatar and password

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useState } from "react";
 
 import Button from "../../ui/Button";
@@ -7,6 +8,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -17,17 +19,32 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isUpdatingUser } = useUpdateUser();
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
     <Form onSubmit={handleSubmit}>
       <FormRow label="Email address">
-        <Input value={email} disabled />
+        <Input value={email} disabled={isUpdatingUser} />
       </FormRow>
       <FormRow label="Full name">
         <Input
@@ -35,6 +52,7 @@ function UpdateUserDataForm() {
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
+          disabled={isUpdatingUser}
         />
       </FormRow>
       <FormRow label="Avatar image">
@@ -42,13 +60,19 @@ function UpdateUserDataForm() {
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
+          disabled={isUpdatingUser}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          variation="secondary"
+          disabled={isUpdatingUser}
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdatingUser}>Update account</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
@@ -47,6 +47,36 @@ export async function signup({ fullName, email, password }) {
   });
 
   if (error) throw new Error(error.message);
-
   return data;
+}
+
+// Update the current user info
+export async function updateCurrentUser({ fullName, password, avatar }) {
+  let updateData;
+  if (password) updateData = password;
+  if (fullName) updateData = { data: { fullName } };
+
+  // 1.) Update password or fullName
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2.) Upload the avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(/* Name of the file */ fileName, /* File itself */ avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3.) Update avatar in the user
+  const { data: updatedUser, error: updatedUserError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+  if (updatedUserError) throw new Error(storageError.message);
+
+  return updatedUser;
 }
